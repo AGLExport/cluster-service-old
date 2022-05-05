4e8b522901132341ef4dd982f1723d687bf0cf2b
Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
2022-05-05 12:43:24 +0900

    Working commit
    Signed-off-by: Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>

--- tools/check-codingstyle.sh
+++ tools/check-codingstyle.sh
@@ -2,3 +2,8 @@
 
 clang-format --dry-run src/*.c
 clang-format --dry-run src/*.h
+clang-format --dry-run lib/*.c
+clang-format --dry-run lib/*.h
+clang-format --dry-run example/*.c
+clang-format --dry-run example/*.h
+clang-format --dry-run include/*.h
